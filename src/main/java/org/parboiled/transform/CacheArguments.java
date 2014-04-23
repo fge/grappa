5c071cdb96a9befae4f7f165817690d42a70813e
Francis Galiegue <fgaliegue@gmail.com>
2014-04-23 13:46:19 +0200

    CacheArguments: simplify logic
    Don't unroll the elements. Use Arrays.deep{HashCode,Equals}() instead.

--- src/main/java/org/parboiled/transform/CacheArguments.java
+++ src/main/java/org/parboiled/transform/CacheArguments.java
@@ -1,14 +1,9 @@
 package org.parboiled.transform;
 
-import org.objectweb.asm.Type;
 import org.parboiled.annotations.Cached;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
-
-import static org.parboiled.common.Utils.toObjectArray;
 
 /**
  * Runtime cache arguments for generated parsers
@@ -26,77 +21,25 @@ public final class CacheArguments
 
     public CacheArguments(final Object... params)
     {
-        // we need to "unroll" all inner Object arrays
-        final List<Object> list = new ArrayList<Object>(params.length);
-        unroll(params, list);
-        this.params = list.toArray();
-    }
-
-    private static void unroll(final Object[] params, final List<Object> list)
-    {
-        Class<?> c;
-        int type;
-        for (final Object param: params) {
-            if (param == null)
-                continue;
-            c = param.getClass();
-            if (!c.isArray()) {
-                list.add(param);
-                continue;
-            }
-            type = Type.getType(c.getComponentType()).getSort();
-            switch (type)
-            {
-                case Type.BOOLEAN:
-                    unroll(toObjectArray((boolean[]) param), list);
-                    continue;
-                case Type.BYTE:
-                    unroll(toObjectArray((byte[]) param), list);
-                    continue;
-                case Type.CHAR:
-                    unroll(toObjectArray((char[]) param), list);
-                    continue;
-                case Type.DOUBLE:
-                    unroll(toObjectArray((double[]) param), list);
-                    continue;
-                case Type.FLOAT:
-                    unroll(toObjectArray((float[]) param), list);
-                    continue;
-                case Type.INT:
-                    unroll(toObjectArray((int[]) param), list);
-                    continue;
-                case Type.LONG:
-                    unroll(toObjectArray((long[]) param), list);
-                    continue;
-                case Type.SHORT:
-                    unroll(toObjectArray((short[]) param), list);
-                    continue;
-                case Type.OBJECT:
-                case Type.ARRAY:
-                    unroll((Object[]) param, list);
-                    continue;
-                default:
-                    throw new IllegalStateException();
-            }
-        }
+        this.params = Arrays.copyOf(params, params.length);
     }
 
     @Override
-    public boolean equals(final Object o)
+    public boolean equals(final Object obj)
     {
-        if (o == null)
+        if (obj == null)
             return false;
-        if (this == o)
+        if (this == obj)
             return true;
-        if (getClass() != o.getClass())
+        if (getClass() != obj.getClass())
             return false;
-        final CacheArguments other = (CacheArguments) o;
-        return Arrays.equals(params, other.params);
+        final CacheArguments other = (CacheArguments) obj;
+        return Arrays.deepEquals(params, other.params);
     }
 
     @Override
     public int hashCode()
     {
-        return Arrays.hashCode(params);
+        return Arrays.deepHashCode(params);
     }
 }
