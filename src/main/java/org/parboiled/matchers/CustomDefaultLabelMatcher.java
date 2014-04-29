83d1aad76afe10e8ed1f86e6045a4049e1650b89
Francis Galiegue <fgaliegue@gmail.com>
2014-04-29 17:20:30 +0200

    CustomDefaultLabelMatcher: make public

--- src/main/java/org/parboiled/matchers/CustomDefaultLabelMatcher.java
+++ src/main/java/org/parboiled/matchers/CustomDefaultLabelMatcher.java
@@ -18,32 +18,39 @@ package org.parboiled.matchers;
 
 import org.parboiled.Rule;
 
-abstract class CustomDefaultLabelMatcher<T extends CustomDefaultLabelMatcher<T>> extends AbstractMatcher {
+public abstract class CustomDefaultLabelMatcher<T extends CustomDefaultLabelMatcher<T>>
+    extends AbstractMatcher
+{
     private String defaultLabel;
 
-    protected CustomDefaultLabelMatcher(Rule subRule, String defaultLabel) {
+    protected CustomDefaultLabelMatcher(Rule subRule, String defaultLabel)
+    {
         super(subRule, null);
         this.defaultLabel = defaultLabel;
     }
 
-    protected CustomDefaultLabelMatcher(Rule[] subRules, String defaultLabel) {
+    protected CustomDefaultLabelMatcher(Rule[] subRules, String defaultLabel)
+    {
         super(subRules, null);
         this.defaultLabel = defaultLabel;
     }
 
     @Override
-    public String getLabel() {
+    public String getLabel()
+    {
         return hasCustomLabel() ? super.getLabel() : defaultLabel;
     }
 
     @Override
-    public boolean hasCustomLabel() {
+    public boolean hasCustomLabel()
+    {
         return super.getLabel() != null;
     }
 
     @SuppressWarnings("unchecked")
-    public T defaultLabel(String defaultLabel) {
+    public T defaultLabel(String defaultLabel)
+    {
         this.defaultLabel = defaultLabel;
-        return (T)this;
+        return (T) this;
     }
 }
