437548393def69e13777a44244d989a132eeb5af
Francis Galiegue <fgaliegue@gmail.com>
2014-05-09 19:54:00 +0200

    Create CharSourceWithSink
    Required while Sink<?> is still there. Makes for easier backwards compatible
    changes as well.

--- src/main/java/com/github/parboiled1/grappa/misc/SystemOutCharSource.java
+++ src/main/java/com/github/parboiled1/grappa/misc/SystemOutCharSource.java
@@ -1,15 +1,13 @@
 package com.github.parboiled1.grappa.misc;
 
 import com.google.common.io.CharSink;
-import org.parboiled.common.Sink;
 
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 
 public final class SystemOutCharSource
-    extends CharSink
-    implements Sink<CharSequence>
+    extends CharSourceWithSink
 {
     public static final CharSink INSTANCE = new SystemOutCharSource();
 
@@ -32,15 +30,5 @@ public final class SystemOutCharSource
         throws IOException
     {
         return new OutputStreamWriter(System.out);
-    }
-
-    @Override
-    public void receive(final CharSequence value)
-    {
-        try {
-            write(value);
-        } catch (IOException e) {
-            throw new RuntimeException("cannot write to sink", e);
-        }
     }
 }
