1badc1b42a31ca86ad70a0a70ed4666aa720e4cb
Francis Galiegue <fgaliegue@gmail.com>
2014-05-10 14:43:13 +0200

    Make TestParser extend JoinParser instead of BaseParser
    Future tests will involve the join() rule.

--- src/test/java/com/github/parboiled1/grappa/testparsers/TestParser.java
+++ src/test/java/com/github/parboiled1/grappa/testparsers/TestParser.java
@@ -1,12 +1,12 @@
 package com.github.parboiled1.grappa.testparsers;
 
-import org.parboiled.BaseParser;
+import org.parboiled.JoinParser;
 import org.parboiled.Rule;
 import org.parboiled.annotations.BuildParseTree;
 
 @BuildParseTree
 public abstract class TestParser<V>
-    extends BaseParser<V>
+    extends JoinParser<V>
 {
     public abstract Rule mainRule();
 }
