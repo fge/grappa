4036d2caf9d46f68b0b9eb61aedf47e07e2f155d
Francis Galiegue <fgaliegue@gmail.com>
2015-01-05 14:19:26 +0100

    VarFramingGenerator: use CodeBlock

--- src/main/java/org/parboiled/transform/process/VarFramingGenerator.java
+++ src/main/java/org/parboiled/transform/process/VarFramingGenerator.java
@@ -16,18 +16,12 @@
 
 package org.parboiled.transform.process;
 
-import com.github.parboiled1.grappa.annotations.WillBeFinal;
+import com.github.parboiled1.grappa.transform.CodeBlock;
 import com.google.common.base.Preconditions;
 import me.qmx.jitescript.util.CodegenUtils;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.InsnList;
-import org.objectweb.asm.tree.InsnNode;
-import org.objectweb.asm.tree.IntInsnNode;
-import org.objectweb.asm.tree.LdcInsnNode;
 import org.objectweb.asm.tree.LocalVariableNode;
-import org.objectweb.asm.tree.MethodInsnNode;
-import org.objectweb.asm.tree.TypeInsnNode;
-import org.objectweb.asm.tree.VarInsnNode;
 import org.parboiled.Rule;
 import org.parboiled.matchers.VarFramingMatcher;
 import org.parboiled.support.Var;
@@ -36,24 +30,13 @@ import org.parboiled.transform.RuleMethod;
 
 import javax.annotation.Nonnull;
 
-import static org.objectweb.asm.Opcodes.AASTORE;
-import static org.objectweb.asm.Opcodes.ALOAD;
-import static org.objectweb.asm.Opcodes.ANEWARRAY;
 import static org.objectweb.asm.Opcodes.ARETURN;
-import static org.objectweb.asm.Opcodes.BIPUSH;
-import static org.objectweb.asm.Opcodes.DUP;
-import static org.objectweb.asm.Opcodes.DUP_X1;
-import static org.objectweb.asm.Opcodes.INVOKESPECIAL;
-import static org.objectweb.asm.Opcodes.INVOKEVIRTUAL;
-import static org.objectweb.asm.Opcodes.NEW;
-import static org.objectweb.asm.Opcodes.SWAP;
 
 /**
  * Inserts code for wrapping the created rule into a VarFramingMatcher if the
  * method contains local variables assignable to {@link Var}.
  */
-@WillBeFinal(version = "1.1")
-public class VarFramingGenerator
+public final class VarFramingGenerator
     implements RuleMethodProcessor
 {
 
@@ -79,57 +62,44 @@ public class VarFramingGenerator
         while (ret.getOpcode() != ARETURN)
             ret = ret.getPrevious();
 
+        final CodeBlock block = CodeBlock.newCodeBlock();
 
-        // stack: <Matcher>
-        instructions.insertBefore(ret, new TypeInsnNode(NEW,
-            CodegenUtils.p(VarFramingMatcher.class)));
-        // stack: <Matcher> :: <VarFramingMatcher>
-        instructions.insertBefore(ret, new InsnNode(DUP_X1));
-        // stack: <VarFramingMatcher> :: <Matcher> :: <VarFramingMatcher>
-        instructions.insertBefore(ret, new InsnNode(SWAP));
-        // stack: <VarFramingMatcher> :: <VarFramingMatcher> :: <Matcher>
-        createVarFieldArray(method, instructions, ret);
-        // stack: <VarFramingMatcher> :: <VarFramingMatcher> :: <Matcher> :: <VarFieldArray>
-        instructions.insertBefore(ret, new MethodInsnNode(INVOKESPECIAL,
-            CodegenUtils.p(VarFramingMatcher.class), "<init>",
-            CodegenUtils.sig(void.class, Rule.class, Var[].class), false));
-        // stack: <VarFramingMatcher>
+        block.newobj(CodegenUtils.p(VarFramingMatcher.class))
+            .dup_x1()
+            .swap();
+
+        createVarFieldArray(block, method);
+
+        block.invokespecial(CodegenUtils.p(VarFramingMatcher.class), "<init>",
+            CodegenUtils.sig(void.class, Rule.class, Var[].class));
+
+        instructions.insertBefore(ret, block.getInstructionList());
 
         method.setBodyRewritten();
     }
 
-    private static void createVarFieldArray(final RuleMethod method,
-        final InsnList instructions, final AbstractInsnNode ret)
+    private static void createVarFieldArray(final CodeBlock block,
+        final RuleMethod method)
     {
         final int count = method.getLocalVarVariables().size();
 
-        // stack:
-        instructions.insertBefore(ret, new IntInsnNode(BIPUSH, count));
-        // stack: <length>
-        instructions.insertBefore(ret, new TypeInsnNode(ANEWARRAY,
-            CodegenUtils.p(Var.class)));
-        // stack: <array>
+        block.bipush(count).anewarray(CodegenUtils.p(Var.class));
+
+        LocalVariableNode var;
+        String varName;
+
         for (int i = 0; i < count; i++) {
-            final LocalVariableNode var = method.getLocalVarVariables().get(i);
-            // stack: <array>
-            instructions.insertBefore(ret, new InsnNode(DUP));
-            // stack: <array> :: <array>
-            instructions.insertBefore(ret, new IntInsnNode(BIPUSH, i));
-            // stack: <array> :: <array> :: <index>
-            instructions.insertBefore(ret, new VarInsnNode(ALOAD, var.index));
-            // stack: <array> :: <array> :: <index> :: <var>
-            instructions.insertBefore(ret, new InsnNode(DUP));
-            // stack: <array> :: <array> :: <index> :: <var> :: <var>
-            instructions.insertBefore(ret,
-                new LdcInsnNode(method.name + ':' + var.name));
-            // stack: <array> :: <array> :: <index> :: <var> :: <var> :: <varName>
-            instructions.insertBefore(ret, new MethodInsnNode(INVOKEVIRTUAL,
-                CodegenUtils.p(Var.class), "setName",
-                CodegenUtils.sig(void.class, String.class), false));
-            // stack: <array> :: <array> :: <index> :: <var>
-            instructions.insertBefore(ret, new InsnNode(AASTORE));
-            // stack: <array>
+            var = method.getLocalVarVariables().get(i);
+            varName = method.name + ':' + var.name;
+
+            block.dup()
+                .bipush(i)
+                .aload(var.index)
+                .dup()
+                .ldc(varName)
+                .invokevirtual(CodegenUtils.p(Var.class), "setName",
+                    CodegenUtils.sig(void.class, String.class))
+                .aastore();
         }
-        // stack: <array>
     }
 }
