49854bd4152f3fdffbbd171854992a25fdb1f407
Francis Galiegue <fgaliegue@gmail.com>
2015-01-25 00:28:23 +0100

    TracingParseRunnerListener: write events in another thread
    Use a LinkedBlockingQueue to send events.

--- src/main/java/com/github/fge/grappa/trace/TracingParseRunnerListener.java
+++ src/main/java/com/github/fge/grappa/trace/TracingParseRunnerListener.java
@@ -20,15 +20,18 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonGenerator.Feature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.fge.grappa.buffers.InputBuffer;
+import com.github.fge.grappa.matchers.MatcherType;
 import com.github.fge.grappa.run.MatchFailureEvent;
 import com.github.fge.grappa.run.MatchSuccessEvent;
 import com.github.fge.grappa.run.ParseRunnerListener;
 import com.github.fge.grappa.run.PostParseEvent;
 import com.github.fge.grappa.run.PreMatchEvent;
 import com.github.fge.grappa.run.PreParseEvent;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.io.BufferedWriter;
+import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
@@ -40,6 +43,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -47,8 +58,16 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 public final class TracingParseRunnerListener<V>
     extends ParseRunnerListener<V>
 {
+    private static final ThreadFactory THREAD_FACTORY
+        = new ThreadFactoryBuilder().setDaemon(true)
+        .setNameFormat("event-writer-%d").build();
+
     private static final Map<String, ?> ZIPFS_ENV
         = Collections.singletonMap("create", "true");
+
+    private static final TraceEvent END_EVENT
+        = new TraceEvent(TraceEventType.BEFORE_MATCH, -1L, 0, "", "",
+            MatcherType.ACTION, "", -1);
 
     /*
      * We have to do that, since we write to a temporary file
@@ -56,6 +75,11 @@ public final class TracingParseRunnerListener<V>
     private static final ObjectMapper MAPPER = new ObjectMapper()
         .disable(Feature.AUTO_CLOSE_TARGET);
     private static final int BUFSIZE = 16384;
+
+    private final ExecutorService executor;
+    private final Future<Closeable> future;
+    private final BlockingQueue<TraceEvent> eventQueue
+        = new LinkedBlockingQueue<>();
 
     private final Path traceFile;
     private final BufferedWriter traceWriter;
@@ -76,10 +100,12 @@ public final class TracingParseRunnerListener<V>
             traceFile = Files.createTempFile("trace", ".json");
             traceWriter = Files.newBufferedWriter(traceFile, UTF_8);
             generator = MAPPER.getFactory().createGenerator(traceWriter);
-            generator.writeStartArray();
         } catch (IOException e) {
             throw new RuntimeException("failed to initialize trace", e);
         }
+
+        executor = Executors.newSingleThreadExecutor(THREAD_FACTORY);
+        future = executor.submit(new EventWriter(generator, eventQueue));
     }
 
     public TracingParseRunnerListener(final String zipPath)
@@ -107,7 +133,7 @@ public final class TracingParseRunnerListener<V>
         final TraceEvent traceEvent = TraceEvent.before(event.getContext());
         final long nanoseconds = System.nanoTime();
         traceEvent.setNanoseconds(nanoseconds);
-        writeEvent(traceEvent);
+        eventQueue.offer(traceEvent);
     }
 
     @Override
@@ -116,7 +142,7 @@ public final class TracingParseRunnerListener<V>
         final long nanos = System.nanoTime();
         final TraceEvent traceEvent = TraceEvent.success(event.getContext());
         traceEvent.setNanoseconds(nanos);
-        writeEvent(traceEvent);
+        eventQueue.offer(traceEvent);
     }
 
     @Override
@@ -125,24 +151,25 @@ public final class TracingParseRunnerListener<V>
         final long nanos = System.nanoTime();
         final TraceEvent traceEvent = TraceEvent.failure(event.getContext());
         traceEvent.setNanoseconds(nanos);
-        writeEvent(traceEvent);
+        eventQueue.offer(traceEvent);
     }
 
     @Override
     public void afterParse(final PostParseEvent<V> event)
     {
-        final ParseRunInfo runInfo = new ParseRunInfo(startDate, inputBuffer);
-
-        final URI uri = URI.create("jar:" + zipPath.toUri());
+        eventQueue.offer(END_EVENT);
 
         try (
-            final JsonGenerator gen = generator;
+            final Closeable gen = future.get();
         ) {
-            gen.writeEndArray();
             traceWriter.flush();
-        } catch (IOException e) {
-            throw new RuntimeException("failed to write event file", e);
+            executor.shutdown();
+        } catch (InterruptedException | ExecutionException | IOException e) {
+            throw new RuntimeException("failed to generate trace file", e);
         }
+
+        final ParseRunInfo runInfo = new ParseRunInfo(startDate, inputBuffer);
+        final URI uri = URI.create("jar:" + zipPath.toUri());
 
         try (
             final FileSystem zipfs = FileSystems.newFileSystem(uri, ZIPFS_ENV);
@@ -197,6 +224,35 @@ public final class TracingParseRunnerListener<V>
             }
 
             writer.flush();
+        }
+    }
+
+    private static final class EventWriter
+        implements Callable<Closeable>
+    {
+        private final JsonGenerator generator;
+        private final BlockingQueue<TraceEvent> eventQueue;
+
+        private EventWriter(final JsonGenerator generator,
+            final BlockingQueue<TraceEvent> eventQueue)
+        {
+            this.generator = generator;
+            this.eventQueue = eventQueue;
+        }
+
+        @Override
+        public Closeable call()
+            throws IOException
+        {
+            TraceEvent event;
+            generator.writeStartArray();
+            try {
+                while ((event = eventQueue.take()) != END_EVENT)
+                    generator.writeObject(event);
+                generator.writeEndArray();
+            } catch (InterruptedException ignored) {
+            }
+            return generator;
         }
     }
 }
