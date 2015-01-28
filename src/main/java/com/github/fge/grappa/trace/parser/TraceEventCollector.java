784d2be606072a57e8ffa6a166865a5b926091d3
Francis Galiegue <fgaliegue@gmail.com>
2015-01-28 16:13:26 +0100

    Code to collect events in a list (fixed quantity!)

--- src/main/java/com/github/fge/grappa/trace/parser/TraceEventCollector.java
+++ src/main/java/com/github/fge/grappa/trace/parser/TraceEventCollector.java
@@ -1,5 +1,24 @@
 package com.github.fge.grappa.trace.parser;
 
+import com.github.fge.grappa.trace.TraceEvent;
+import com.google.common.eventbus.Subscribe;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public final class TraceEventCollector
 {
+    private final List<TraceEvent> events = new ArrayList<>();
+
+    @Subscribe
+    public void receiveEvent(final TraceEvent event)
+    {
+        events.add(event);
+    }
+
+    public List<TraceEvent> getEvents()
+    {
+        return Collections.unmodifiableList(events);
+    }
 }
