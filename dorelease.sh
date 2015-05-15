ce3ce05d54a44ee94e786dd3620f2547a767129c
Francis Galiegue <fgaliegue@gmail.com>
2015-05-15 14:55:26 +0200

    Don't use the gradle daemon when performing releases

--- dorelease.sh
+++ dorelease.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-./gradlew --recompile-scripts clean test uploadArchives
+#
+# We don't want to use the gradle daemon (new in 2.4) for releases
+#
+
+./gradlew --no-daemon --recompile-scripts clean test uploadArchives
 
